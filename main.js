763f6892fc9942e778b9273cb3a14f595fa3d264
Mark Fantini <strictlybusinessemail242@gmail.com>
2018-11-21 09:35:41 -0700

    Does this work?

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 
->>>>>>> 68a95fe80268ab61ff1587d8d1674ce0af44c674
 function  🍞💩🍞() {
   console.log('dookieSammich')
 }
@@ -11,30 +8,14 @@ function  🍞💩🍞() {
 function whatsYourFunction(functions){
   return whatsYourFunction(functions);
 }
-<<<<<<< HEAD
-document.addEventListener("DOMContentLoaded",()=>{
-  let bdy = document.getElementsByTagName("body")[0]
-  let h1=querySelector("Seth's-Dookie-Sammie");
-  bdy.addEventListener("click",(e)=>{
-    e.target.style.animation = "spin 15s linear infinite"
-  })
-  h1.addEventListener("mouseover",e=>h1.innerText==="Seth Did This");
-
-})
-=======
-
 document.addEventListener("DOMContentLoaded",()=>{
   let bdy = document.getElementsByTagName("body")
+  let h1=querySelector("Seth's-Dookie-Sammie");
   for(let i = 0; i<bdy.length; i++){
     bdy[i].addEventListener("click",(e)=>{
       e.target.style.animation = "spin 15s linear infinite"
     })
   }
+  h1.addEventListener("mouseover",e=>h1.innerText==="Seth Did This");
+
 })
-
-
-function dookieSammich () {
-  console.log('🍞💩🍞')
-}
-dookieSammich()
->>>>>>> 68a95fe80268ab61ff1587d8d1674ce0af44c674
