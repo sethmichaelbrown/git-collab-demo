a1b3194a901bf6460834e0c3580c9deed8b3663d
Mark Fantini <strictlybusinessemail242@gmail.com>
2018-11-21 09:48:36 -0700

    Whew

--- main.js
+++ main.js
@@ -10,12 +10,13 @@ function whatsYourFunction(functions){
 }
 document.addEventListener("DOMContentLoaded",()=>{
   let bdy = document.getElementsByTagName("body")
-  let h1=querySelector("Seth's-Dookie-Sammie");
+  let h1=document.querySelector(".seth");
   for(let i = 0; i<bdy.length; i++){
     bdy[i].addEventListener("click",(e)=>{
       e.target.style.animation = "spin 15s linear infinite"
     })
   }
-  h1.addEventListener("mouseover",e=>h1.innerText==="Seth Did This");
+  h1.addEventListener("mouseover",()=>h1.innerText="Seth Did This");
+  h1.addEventListener("mouseout",()=>h1.innerText="Paul Did This");
 
 })
